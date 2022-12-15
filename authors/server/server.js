614197130b376836399bcdf9698ada67e655676b
Artan Merko <artanmerko@gmail.com>
2022-12-15 17:25:05 -0500

    updated components

--- authors/server/server.js
+++ authors/server/server.js
@@ -1,9 +1,10 @@
 const express =  require('express');
 const app = express();
 const port = 8000;
+const cors = require('cors');
 
 require('./config/mongoose.config');
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
