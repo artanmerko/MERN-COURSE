f76ce87ab867497fe2737d07ba3088602d6850e0
Artan Merko <artanmerko@gmail.com>
2022-12-15 17:00:06 -0500

    updated config

--- authors/server/config/mongoose.config.js
+++ authors/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/crmdb", {
+mongoose.connect("mongodb://localhost/author", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
